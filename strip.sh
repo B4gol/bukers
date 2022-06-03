58eb69686bbdc0972f10f2032020d4dd44036ec1
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 11:46:51 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,6 +1,6 @@
 # Main Environment
 KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/zImage-dtb
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$HOME/buildkernel/AnyKernel
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
 CONFIG=riva_defconfig
@@ -10,7 +10,7 @@ CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 cp $KERN_IMG $ZIP_DIR
 cd $ZIP_DIR
-mv zImage-dtb zImage
+mv Image-dtb Image.gz-dtb
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 zip -r $zip_name.zip ./*
