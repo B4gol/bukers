d98d2015f0510b962c06f36a13dc52f3cb3d4cfb
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 19:59:26 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -7,17 +7,21 @@ CONFIG=final_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$PWD/gcc/bin/aarch64-elf-gcc-"
+CROSS_COMPILE32+="ccache32 "
+CROSS_COMPILE+="$PWD/gcc/bin/aarch64-elf-"
+CROSS_COMPILE32+="$PWD/gcc/bin/arm-eabi-"
+export CROSSC=$CROSS_COMPILE
+export CROSSC32=$CROSS_COMPILE32
 LD="ld.lld"
 
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
 git clone https://github.com/mvaisakh/gcc-arm64.git gcc --depth=1
-#git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git aarch64-linux-android-4.9
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
 chmod a+x /home/circleci/kernel/gcc/bin/*
-chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf-gcc/*
-chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf-gcc/plugin/*
+chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf/13.0.0/*
+chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf/13.0.0/plugin/*
 
 #cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
 #cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
@@ -28,14 +32,15 @@ Product : Rova Kernel
 Device  : #riva
 Branch  : R11
 Compiler: ""$(gcc --version | head -n 1)""
-Jalur   : ""$($CROSS_COMPILE-$LD --version | head -n 1)""
+Jalur   : ""$($CROSSC-$LD --version | head -n 1)""
 Date    : ""$(env TZ=Asia/Jakarta date)"""
 
 # Export
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH=/usr/bin:$CROSS_COMPILE:$LD:/usr/lib/ccache:$PATH
+export PATH=/usr/bin:$CROSSC:$CROSSC32:$LD:/usr/lib/ccache:$PATH
 export CROSS_COMPILE
+export CROSS_COMPILE32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCi-Agent
 make O=out $CONFIG $THREAD
