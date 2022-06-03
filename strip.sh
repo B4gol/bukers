749b4bf52c9f4f137be6c448997ea6f7ab5edc3b
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 21:30:14 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -9,8 +9,8 @@ THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="~/gcc/bin/aarch64-elf-"
 CROSS_COMPILE_ARM32+="~/gcc/bin/arm-eabi-"
-export CROSSC=$CROSS_COMPILE
-export CROSSC32=$CROSS_COMPILE_ARM32
+export CROSSC="~/gcc64/bin/aarch64-elf-"
+export CROSSC32="~/gcc32/bin/arm-eabi-"
 LD="ld.lld"
 cp $KERN_IMG $ZIP_DIR
 cd $ZIP_DIR
