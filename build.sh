a0f654fee7f69853553455fde2d5e95965c5667f
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 23:21:29 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -8,21 +8,21 @@ CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
-git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
+#git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
 #git clone https://github.com/mvaisakh/gcc-arm.git --depth=1 ~/gcc32
-#git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git aarch64-linux-android-4.9
-#git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
-chmod a+x $HOME/gcc64/bin/*
-#chmod a+x $HOME/gcc32/libexec/gcc/aarch64-elf/13.0.0/*
-chmod a+x $HOME/gcc64/libexec/gcc/aarch64-elf/13.0.0/plugin/*
+git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git kernel/gcc64
+git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git kernel/gcc32
+sudo chmod a+x /home/circleci/kernel/gcc64/bin/*
+sudo chmod a+x /home/circleci/kernel/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
+sudo chmod a+x /home/circleci/kernel/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
+
 #cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
 #cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
 
-#CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$HOME/gcc64/bin/aarch64-elf-"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$KERNEL_DIR/gcc64/bin/aarch64-linux-android-"
 #CROSS_COMPILE_ARM32+="$HOME/gcc32/bin/arm-eabi-"
 CROSSC="${CROSS_COMPILE}"
-#export CROSSC32+="$HOME/gcc32/bin/arm-eabi-"
 
 $HOME/buildkernel/telegram -M "Build Start
 By: B4gol
@@ -35,7 +35,7 @@ Tanggal: ""$(env TZ=Asia/Jakarta date)"""
 # Export
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH=/usr/bin:$CROSSC:$PATH
+export PATH=/usr/bin:ccache:$CROSSC:$PATH
 export CROSS_COMPILE
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCi-Agent
