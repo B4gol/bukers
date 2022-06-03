a2880d0058799c0ab24dffdcd9937d8b70c427db
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 23:49:30 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -4,18 +4,20 @@ KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$HOME/buildkernel/AnyKernel
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-CONFIG=final_defconfig
+CONFIG=riva_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="$KERNEL_DIR/gcc64/bin/aarch64-linux-android-"
-#CROSS_COMPILE_ARM32+="$HOME/gcc32/bin/arm-eabi-"
-CROSSC="${CROSS_COMPILE}"
+CROSS_COMPILE_ARM32+="$KERNEL_DIR/gcc32/bin/arm-linux-androideabi-"
+CROSSC=aarch64-linux-android-
+CROSSC32=arm-linux-androideabi-
 # Export
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH=/usr/bin:ccache:$CROSSC:$PATH
+export PATH=/usr/bin:usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 export CROSS_COMPILE
+export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCi-Agent
 
