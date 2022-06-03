438096370b926c9cc5abb806117aa54a83407498
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 20:45:20 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -6,25 +6,27 @@ CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
 CONFIG=final_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
-CROSS_COMPILE+="ccache "
-CROSS_COMPILE32+="ccache32 "
-CROSS_COMPILE+="$PWD/gcc/bin/aarch64-elf-"
-CROSS_COMPILE32+="$PWD/gcc/bin/arm-eabi-"
-export CROSSC=$CROSS_COMPILE
-export CROSSC32=$CROSS_COMPILE32
-LD="ld.lld"
 
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
-git clone https://github.com/mvaisakh/gcc-arm64.git gcc --depth=1
-git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
+git clone https://github.com/mvaisakh/gcc-arm.git --depth=1 ~/gcc32
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git aarch64-linux-android-4.9
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
-chmod a+x /home/circleci/kernel/gcc/bin/*
-chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf/13.0.0/*
-chmod a+x /home/circleci/kernel/gcc/libexec/gcc/aarch64-elf/13.0.0/plugin/*
-
+chmod a+x ~/gcc64/bin/*
+chmod a+x ~/gcc32/libexec/gcc/aarch64-elf/13.0.0/*
+chmod a+x ~/gcc64/libexec/gcc/aarch64-elf/13.0.0/plugin/*
+chmod a+x ~/gcc32/bin/*
+chmod a+x ~/gcc32/libexec/gcc32/arm-eabi/13.0.0/*
+chmod a+x ~/gcc32/libexec/gcc32/arm-eabi/13.0.0/plugin/*
 #cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
 #cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
+
+CROSS_COMPILE+="ccache"
+CROSS_COMPILE+="~/gcc64/bin/aarch64-elf-"
+CROSS_COMPILE_ARM32+="~/gcc32/bin/arm-eabi-"
+export CROSSC=$CROSS_COMPILE
+export CROSSC32=$CROSS_COMPILE_ARM32
+LD="ld.lld"
 
 $HOME/buildkernel/telegram -M "Build Start
 By      : B4gol
@@ -40,7 +42,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export PATH=/usr/bin:$CROSSC:$CROSSC32:$LD:/usr/lib/ccache:$PATH
 export CROSS_COMPILE
-export CROSS_COMPILE32
+export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCi-Agent
-make O=out $CONFIG $THREAD
+make O=out ARCH=$ARCH $CONFIG $THREAD
