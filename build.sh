080a291ca1d0ff49912a3747f5d84c951834809d
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 23:37:57 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # Main Environment
+cd $HOME/kernel
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel
@@ -10,8 +11,8 @@ THREAD="-j$CORES"
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
 #git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
 #git clone https://github.com/mvaisakh/gcc-arm.git --depth=1 ~/gcc32
-git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $KERNEL_DIR/gcc64
-git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git kernel/gcc32
+git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git gcc64
+git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git gcc32
 sudo chmod a+x $KERNEL_DIR/gcc64/bin/*
 sudo chmod a+x $KERNEL_DIR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
 sudo chmod a+x $KERNEL_DIR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
