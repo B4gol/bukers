f0da61cedff51d8b48986e7d54b574166c261539
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 12:48:35 +0700

    update c.sh

--- c.sh
+++ c.sh
@@ -2,16 +2,10 @@
 #Download : ["$zip_name".zip](https://github.com/B4gol/kernelscript/releases/download/"$CIRCLECI_TAG"/"$zip_name".zip)"
 
 curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@$HOME/buildkernel/AnyKernel/$zip_name.zip -F "parse_mode=html" -F caption="Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-Dev : B4gol
-
-Product : Kernel
-
-Device : #riva
-
-Branch : Rel
-
-Compiler : ""$(gcc --version)""
-
-Compiler : ""$(${CROSS_COMPILE}gcc --version | head -n 1)""
-
-Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
+By      : B4gol
+Product : qcom
+Device  : #riva
+Branch  : R11
+Comp    : ""$(gcc --version)""
+Compiler: ""$(${CROSS_COMPILE}gcc --version | head -n 1)""
+Date    : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
