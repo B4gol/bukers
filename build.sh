88ad26620905de3adca078d7384af452ab7cfe43
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-03 22:35:12 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git aarch64-linux-android-4.9
 #git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git arm-linux-androideabi-4.9
 chmod a+x $HOME/gcc64/bin/*
-chmod a+x $HOME/gcc32/libexec/gcc/aarch64-elf/13.0.0/*
+#chmod a+x $HOME/gcc32/libexec/gcc/aarch64-elf/13.0.0/*
 chmod a+x $HOME/gcc64/libexec/gcc/aarch64-elf/13.0.0/plugin/*
 #cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
 #cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
@@ -36,7 +36,7 @@ Tanggal: ""$(env TZ=Asia/Jakarta date)"""
 export ARCH=arm64
 export SUBARCH=arm64
 export PATH=/usr/bin:$CROSSC:$PATH
-export $CROSSC
+export CROSS_COMPILE
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCi-Agent
 make O=out $CONFIG $THREAD
