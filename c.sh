deaeb484eaa5562b512d0696cd96ef1bfd73aae9
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-11 23:02:47 +0700

    update c.sh

--- c.sh
+++ c.sh
@@ -1,5 +1,5 @@
 #$HOME/buildkernel/telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 #Download : ["$zip_name".zip](https://github.com/B4gol/kernelscript/releases/download/"$CIRCLECI_TAG"/"$zip_name".zip)"
 
-curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@/home/circleci/factor/AnyKernel/$zip_name.zip -F "parse_mode=html" -F caption="Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
+curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@$HOME/AnyKernel/$zip_name.zip -F "parse_mode=html" -F caption="Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
