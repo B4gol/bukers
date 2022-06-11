50a1c18bfb5bccef7b32a474a230593d78dfe8c9
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 00:32:29 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -6,11 +6,11 @@ CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="$FACTOR/kernel/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="$FACTOR/kernel/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
-export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
+PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 # Export
 export ARCH=arm
 export SUBARCH=arm
@@ -26,3 +26,9 @@ mv Image.gz-dtb Image.gz-dtb
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 zip -r $zip_name.zip ./*
+
+#$HOME/buildkernel/telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
+#Download : ["$zip_name".zip](https://github.com/B4gol/kernelscript/releases/download/"$CIRCLECI_TAG"/"$zip_name".zip)"
+
+curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@$HOME/kernelscript/AnyKernel/$zip_name.zip -F "parse_mode=html" -F caption="Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
+Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
