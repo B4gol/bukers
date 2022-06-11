dcbdb92711f8f3615b438b40f2f718c90ed8c475
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 06:09:54 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ ZIP_DIR=$FACTORY/keript/AnyKernel
 CONFIG_DIR=$FACTORY/arch/arm/configs
 echo "Chmod Path"
 # gcc64
-sudo chmod a+x $FACTORY/kdirect/gcc64/bin/*
-sudo chmod a+x $FACTORY/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
-sudo chmod a+x $FACTORY/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
+sudo chmod a+x $HOME/kdirect/gcc64/bin/*
+sudo chmod a+x $HOME/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
+sudo chmod a+x $HOME/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
 # gcc32
-sudo chmod a+x $FACTORY/kdirect/gcc32/bin/*
-sudo chmod a+x $FACTORY/kdirect/gcc64/libexec/gcc/arm-linux-androideabi/4.9.x/*
-sudo chmod a+x $FACTORY/kdirect/gcc64/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
+sudo chmod a+x $HOME/kdirect/gcc32/bin/*
+sudo chmod a+x $HOME/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/*
+sudo chmod a+x $HOME/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$FACTORY/kdirect/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$FACTORY/kdirect/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="kdirect/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="kdirect/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
 $FACTORY/keript/telegram -M "Build
