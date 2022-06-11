9dd7302c4f01ab22e3c9d82e4d48eef9046f31ab
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 02:54:17 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 # Main Environment
 FACTOR=$pwd
-ZIP_DIR=$FACTOR/kernelscript/AnyKernel
-KERN_IMG=$FACTOR/kernel/out/arch/arm/boot/Image.gz-dtb
 CONFIG_DIR=$FACTOR/kernel/arch/arm/configs
 CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
