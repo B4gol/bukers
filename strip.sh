98a98980eeb9e9ade04b85786d27940304653aab
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 04:28:32 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,21 +1,17 @@
-# Main Environment
-ZIP_DIR=$HOME/kernelscript/AnyKernel
-KERN_IMG=$HOME/kdirect/out/arch/arm/boot/Image.gz-dtb
-CONFIG_DIR=$FACTOR/kdirect/arch/arm/configs
-CONFIG=viper_defconfig
-CORES=$(grep -c ^processor /proc/cpuinfo)
-THREAD="-j$CORES"
+# Path Setup
+KDIRECT=$pwd
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$FACTOR/kernel/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$FACTOR/kernel/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="$KDIRECT/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="$KDIRECT/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
-PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
-# Export
+# Export Path
+export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCI
-####
+# Main Environment
+##
