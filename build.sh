20ab61d36724f83d08570ce4283449f5febebd29
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 03:27:42 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,20 @@
-# Main Environment
-FACTOR=$pwd
-CONFIG_DIR=$FACTOR/kernel/arch/arm/configs
-CONFIG=viper_defconfig
-CORES=$(grep -c ^processor /proc/cpuinfo)
-THREAD="-j$CORES"
-
+# Path Setup
 CROSS_COMPILE+="ccache "
 CROSS_COMPILE+="$FACTOR/kernel/gcc64/bin/aarch64-linux-android-"
 CROSS_COMPILE_ARM32+="$FACTOR/kernel/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
+# Export Path
 export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
-
-# Export
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCI
+# Main Environment
+CONFIG=viper_defconfig
+CORES=$(grep -c ^processor /proc/cpuinfo)
+THREAD="-j$CORES"
 make O=out $CONFIG $THREAD
+##
