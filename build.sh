3192e460cb084a92931987a35579d36af952325a
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 03:42:12 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 # Path Setup
+HOME=$pwd
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$FACTOR/kernel/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$FACTOR/kernel/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="$HOME/kdirect/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="$HOME/kdirect/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
 # Export Path
