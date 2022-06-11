6dd538c1e5d8833b10d76e0a7ac1fa469ae5c3a4
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-11 23:52:23 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,15 @@
 # Main Environment
 export FACTOR=$PWD
-KERN_IMG=$FACTOR/out/arch/arm/boot/Image.gz-dtb
+KERN_IMG=$FACTOR/kernel/out/arch/arm/boot/Image.gz-dtb
 ZIP_DIR=$FACTOR/AnyKernel
-CONFIG_DIR=$FACTOR/arch/arm/configs
+CONFIG_DIR=$FACTOR/kernel/arch/arm/configs
 CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 
-echo -e "\n(i) Cloning toolcahins if folder not exist..."
-#git clone https://github.com/mvaisakh/gcc-arm64 --depth=1 ~/gcc64
-#git clone https://github.com/mvaisakh/gcc-arm.git --depth=1 ~/gcc32
-git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $FACTOR/gcc64
-git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git $FACTOR/gcc32
-sudo chmod a+x $FACTOR/gcc64/bin/*
-sudo chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
-sudo chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
-
-#cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
-#cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
-
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$FACTOR/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$FACTOR/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
 
