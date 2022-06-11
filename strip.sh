34950c79f8570b448f9caee8ce01ce72d17f5dfa
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 00:09:29 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,6 +1,5 @@
 # Main Environment
 export FACTOR=$PWD
-KERN_IMG=$FACTOR/kernel/out/arch/arm/boot/Image.gz-dtb
 ZIP_DIR=$FACTOR/AnyKernel
 CONFIG_DIR=$FACTOR/kernel/arch/arm/configs
 CONFIG=viper_defconfig
@@ -11,14 +10,15 @@ CROSS_COMPILE+="gcc64/bin/aarch64-linux-android-"
 CROSS_COMPILE_ARM32+="gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
+export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 # Export
 export ARCH=arm
 export SUBARCH=arm
-export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCI
+export KERN_IMG=$FACTOR/kernel/out/arch/arm/boot/Image.gz-dtb
 
 cp $KERN_IMG $ZIP_DIR
 cd $ZIP_DIR
