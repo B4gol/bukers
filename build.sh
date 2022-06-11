c6d352bcf4ec272a3358ee3c41de002ae7b1768c
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-11 23:01:37 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -12,9 +12,9 @@ echo -e "\n(i) Cloning toolcahins if folder not exist..."
 #git clone https://github.com/mvaisakh/gcc-arm.git --depth=1 ~/gcc32
 git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $FACTOR/gcc64
 git clone https://github.com/wulan17/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9.git $FACTOR/gcc32
-  chmod a+x $FACTOR/gcc64/bin/*
-  chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
-  chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
+sudo chmod a+x $FACTOR/gcc64/bin/*
+sudo chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
+sudo chmod a+x $FACTOR/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
 
 #cd $HOME/kernel && wget https://github.com/wulan17/android_kernel_xiaomi_cactus/commit/63623ef9ea9260810d10c2422d4548470a29f304.patch
 #cd $HOME/kernel && git am < 63623ef9ea9260810d10c2422d4548470a29f304.patch
