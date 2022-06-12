5d444f20f5f07bd09449fc38a48addd227c707cb
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 10:43:54 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,22 +1,21 @@
-KDIRECT=$pwd
-KERN_IMG=$KDIRECT/kdirect/out/arch/arm/boot/Image.gz-dtb
-ZIP_DIR=$KDIRECT/AnyKernel
-CONFIG_DIR=$KDIRECT/kdirect/arch/arm/configs
+KERN_IMG=$HOME/kdirect/out/arch/arm/boot/Image.gz-dtb
+ZIP_DIR=$HOME/sdirect/AnyKernel
+CONFIG_DIR=$HOME/kdirect/arch/arm/configs
 CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 echo "Chmod Path"
 # gcc64
-sudo chmod a+x $KDIRECT/kdirect/gcc64/bin/*
-sudo chmod a+x $KDIRECT/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
-sudo chmod a+x $KDIRECT/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
+sudo chmod a+x $HOME/kdirect/gcc64/bin/*
+sudo chmod a+x $HOME/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
+sudo chmod a+x $HOME/kdirect/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
 # gcc32
-sudo chmod a+x $KDIRECT/kdirect/gcc32/bin/*
-sudo chmod a+x $KDIRECT/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/*
-sudo chmod a+x $KDIRECT/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
+sudo chmod a+x $HOME/kdirect/gcc32/bin/*
+sudo chmod a+x $HOME/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/*
+sudo chmod a+x $HOME/kdirect/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$KDIRECT/kdirect/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$KDIRECT/kdirect/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="$HOME/kdirect/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="$HOME/kdirect/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
 # Export
