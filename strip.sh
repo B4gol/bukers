ff8f653c409d745250eadf4093159357d1090fa9
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 15:14:33 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,22 +1,20 @@
 cd /home/circleci/kdirect
-DIRECT=$pwd
-SDIRECT=/home/circleci/sdirect
-KERN_IMG=$DIRECT/out/arch/arm/boot/Image.gz-dtb
-ZIP_DIR=$SDIRECT/AnyKernel
-CONFIG_DIR=$DIRECT/arch/arm/configs
+KERN_IMG=out/arch/arm/boot/Image.gz-dtb
+ZIP_DIR=/home/circleci/sdirect/AnyKernel
+CONFIG_DIR=arch/arm/configs
 CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 # PATH
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$DIRECT/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$DIRECT/gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
 # Export
 export ARCH=arm
 export SUBARCH=arm
-export PATH=/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
+export PATH=/home/circleci/bin:/usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
