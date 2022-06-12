d46f28b4f8b725372669f31f916b5f679f307fca
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 15:32:09 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -33,4 +33,4 @@ export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export KBUILD_BUILD_USER=B4gol
 export KBUILD_BUILD_HOST=CircleCI
-make O=/home/circleci/out $CONFIG $THREAD
+sudo make O=out $CONFIG $THREAD
