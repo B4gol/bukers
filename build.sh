71a41d544564bcc001c280e1f735a638b9ab18fc
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-12 08:12:59 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
-DIRECT="$(pwd)/kdirect"
+DIRECT=$PWD
 KERN_IMG=$DIRECT/out/arch/arm/boot/Image.gz-dtb
-ZIP_DIR=../sdirect/AnyKernel
+ZIP_DIR=$OLDPWD/sdirect/AnyKernel
 CONFIG_DIR=$DIRECT/arch/arm/configs
 CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 echo "Chmod Path"
 # gcc64
-sudo chmod a+x ../gcc64/bin/*
-sudo chmod a+x ../gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
-sudo chmod a+x ../gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
+sudo chmod a+x $OLDPWD/gcc64/bin/*
+sudo chmod a+x $OLDPWD/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/*
+sudo chmod a+x $OLDPWD/gcc64/libexec/gcc/aarch64-linux-android/4.9.x/plugin/*
 # gcc32
-sudo chmod a+x ../gcc32/bin/*
-sudo chmod a+x ../gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/*
-sudo chmod a+x ../gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
+sudo chmod a+x $OLDPWD/gcc32/bin/*
+sudo chmod a+x $OLDPWD/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/*
+sudo chmod a+x $OLDPWD/gcc32/libexec/gcc/arm-linux-androideabi/4.9.x/plugin/*
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="../gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="../gcc32/bin/arm-linux-androideabi-"
+CROSS_COMPILE+="$OLDPWD/gcc64/bin/aarch64-linux-android-"
+CROSS_COMPILE_ARM32+="$OLDPWD/gcc32/bin/arm-linux-androideabi-"
 CROSSC=aarch64-linux-android-
 CROSSC32=arm-linux-androideabi-
-../sdirect/telegram -M "Build
+$OLDPWD/sdirect/telegram -M "Build
 Up: B4gol
 Product: Samsung Kernel
 Device: SM-J320G
