d14234ad0c685fad99d3932eae1cfd15298c1816
B4gol <91882824+B4gol@users.noreply.github.com>
2022-06-13 20:30:40 +0700

    update strip.sh

--- strip.sh
+++ strip.sh
@@ -1,28 +1,17 @@
 # Main Environment
 KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$HOME/buildkernel/AnyKernel
-CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
-CONFIG=riva_defconfig
+KERN_IMG=$KERNEL_DIR/out/arch/arm/boot/zImage-dtb
+ZIP_DIR=$HOME/build_kernel/AnyKernel
+CONFIG_DIR=$KERNEL_DIR/arch/arm/configs
+CONFIG=viper_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
 CROSS_COMPILE+="ccache "
-CROSS_COMPILE+="$KERNEL_DIR/gcc64/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$KERNEL_DIR/gcc32/bin/arm-linux-androideabi-"
-CROSSC=aarch64-linux-android-
-CROSSC32=arm-linux-androideabi-
-# Export
-export ARCH=arm64
-export SUBARCH=arm64
-export PATH=/usr/bin:usr/lib/ccache:$CROSSC:$CROSSC32:$PATH
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
-export KBUILD_BUILD_USER=B4gol
-export KBUILD_BUILD_HOST=CircleCi-Agent
-
+CROSS_COMPILE+="$PWD/arm-gcc/bin/arm-linux-androideabi-"
+CROSS_COMPILE_ARM32+="$PWD/arm-gcc/bin/arm-linux-androideabi-"
 cp $KERN_IMG $ZIP_DIR
 cd $ZIP_DIR
-mv Image.gz-dtb Image.gz-dtb
+mv zImage-dtb zImage
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 zip -r $zip_name.zip ./*
